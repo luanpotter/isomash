39f1f6ec7ddca3e6d97466ca0817c3bb84ee8866
Luan Nico <luanpotter27@gmail.com>
2021-04-22 02:20:27 -0400

    Update Flame and wire up the new camera events

--- game/lib/main.dart
+++ game/lib/main.dart
@@ -1,9 +1,10 @@
+import 'dart:html' show document;
+
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/gestures.dart';
 import 'package:flame/sprite.dart';
 import 'package:flame/extensions.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart' show runApp;
 
 import 'components/character.dart';
@@ -11,16 +12,20 @@ import 'components/path.dart';
 import 'components/selector.dart';
 
 void main() {
+  document.onContextMenu.listen((event) => event.preventDefault());
+
   final game = MyGame();
   runApp(GameWidget(game: game));
 }
 
 class MyGame extends BaseGame
-    with MouseMovementDetector, ScrollDetector, TapDetector {
+    with MouseMovementDetector, ScrollDetector, TapDetector, PanDetector {
   late final IsometricTileMapComponent map;
   late final Selector selector;
   late final Character player;
   late final Path path;
+
+  Vector2? dragStart;
 
   @override
   Future<void> onLoad() async {
@@ -65,13 +70,23 @@ class MyGame extends BaseGame
 
     final center = map.getBlockPosition(player.block);
     camera.setRelativeOffset(Anchor.center.toVector2());
-    camera.moveTo(center);
-    camera.snap();
+    camera.snapTo(center);
   }
 
   @override
-  void onMouseMove(PointerHoverEvent details) {
-    final pos = camera.screenToWorld(details.position.toVector2());
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+    canvas.save();
+    camera.apply(canvas);
+    final center = map.getBlockPosition(player.block);
+    canvas.renderPoint(center);
+    canvas.restore();
+  }
+
+  @override
+  void onMouseMove(PointerHoverInfo details) {
+    final pos = details.eventPosition.game;
     final block = map.getBlock(pos);
     if (map.containsBlock(block)) {
       selector.block = block;
@@ -79,17 +94,25 @@ class MyGame extends BaseGame
   }
 
   @override
-  void onScroll(PointerScrollEvent event) {
-    print(event.scrollDelta);
+  void onScroll(PointerScrollInfo event) {
+    final zooms = [0.25, 0.5, 1.0, 2.0, 4.0, 10.0];
+    final idx = zooms.indexOf(camera.zoom) - event.scrollDelta.y.sign.toInt();
+    camera.zoom = zooms[idx % zooms.length];
   }
 
   @override
-  void onTapUp(TapUpDetails details) {
-    final pos = camera.screenToWorld(details.localPosition.toVector2());
+  void onTapUp(TapUpInfo details) {
+    final pos = details.eventPosition.game;
     final block = map.getBlock(pos);
     if (map.containsBlock(block)) {
       path.start = player.block;
       path.end = block;
     }
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    camera.translateBy(info.delta / camera.zoom * -1);
+    camera.snap();
   }
 }
